2a583b9708408aadc061c142e1c77a12aad06ccf
WaseemAlBizreh <waseem.bez@gmail.com>
2022-03-29 19:26:09 +0300

    email

---
+++ lib/view/screens/Auth/email_verification.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EmailVerification extends StatelessWidget {
+  const EmailVerification({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
